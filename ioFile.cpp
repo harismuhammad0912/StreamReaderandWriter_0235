2af61503c16f00d30d9f479a0280d43571fb3da6
harismuhammad0912 <muhammadharis091204@gmail.com>
2024-06-04 15:04:02 +0700

    1

---
+++ ioFile.cpp
@@ -0,0 +1,40 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string baris;
+    ofstream outfile;
+    outfile.open("contohfile.txt");
+
+
+cout << ">= Menulis file, \'q\ untuk keluar" << endl;
+
+while (true) {
+    cout << "- ";
+    getline(cin, baris);
+    if (baris == "q") break;
+    outfile << baris << endl;
+    }
+
+    outfile.close();
+
+    ifstream infile;
+
+    infile.open("contohfile.txt");
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+
+    if (infile.is_open())
+    {
+        while (getline(infile, baris))
+        {
+            cout << baris << '\n';
+        }
+    }
+    else cout << "unable to open file";
+    return 0;
+
+}
