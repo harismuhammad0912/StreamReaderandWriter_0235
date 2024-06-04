cf8e8fd8385e8e7b081a829bca362bfb5f9df42f
harismuhammad0912 <muhammadharis091204@gmail.com>
2024-06-04 15:40:52 +0700

    9

---
+++ manualExeption.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 0.5; //melemparkan sebuah interger maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a) {
+        //blok ini akan dieksekusi
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
+    catch (...) {
+        /jika selain interger maka block ini akan di eksekusi/
+        cout << "default Pengecualian dieksekusi" << endl;    
+    }
+
+    return 0;
+}
